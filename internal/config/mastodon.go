bf314a4120e65b1c34ccbaa60d13bab81d2c8dc8
Nakaya <eniehack@outlook.jp>
2025-08-03 23:47:39 +0900

    feat(mastodon): validateでuserIdを自動的に補完するように

--- internal/config/mastodon.go
+++ internal/config/mastodon.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -18,6 +19,11 @@ type MastodonConfig struct {
 	SiteUrl   string `yaml:"site_url"`
 	Name      string `yaml:"name"`
 	IconUrl   string `yaml:"icon_url"`
+	Acct      string `yaml:"acct"`
+}
+
+type MastodonAccountLookupAPIResponse struct {
+	Id string `yaml:"id"`
 }
 
 func (c *MastodonConfig) SetId(id string) {
@@ -44,6 +50,9 @@ func (c *MastodonConfig) CompleteMetadata(ctx context.Context, id string) error 
 	if len(c.SiteUrl) == 0 {
 		return fmt.Errorf("%s: site_url is undefined", id)
 	}
+	if len(c.Acct) == 0 {
+		return fmt.Errorf("%s: acct is undefined", id)
+	}
 	client := new(http.Client)
 	reqUrl, err := url.Parse(c.SiteUrl)
 	if err != nil {
@@ -68,11 +77,28 @@ func (c *MastodonConfig) CompleteMetadata(ctx context.Context, id string) error 
 		c.Name = htmlquery.InnerText(titleElem)
 	}
 	if len(c.SourceUrl) == 0 {
-		feedUrlElem := htmlquery.FindOne(doc, `//link[@rel="alternate" and (@type="application/rss+xml" or @type="application/atom+xml")]/@href`)
-		srcUrl, err := pkgUrl.ResolveAbsUrl(reqUrl, htmlquery.InnerText(feedUrlElem))
+		lookupEndpoint, _ := url.Parse(c.SiteUrl)
+		lookupEndpoint.Path = "/api/v1/accounts/lookup"
+		query := lookupEndpoint.Query()
+		query.Add("acct", c.Acct)
+		lookupEndpoint.RawQuery = query.Encode()
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, lookupEndpoint.String(), nil)
 		if err != nil {
 			return err
 		}
+		req.Header.Set("User-Agent", UserAgent)
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		lookupRespPayload := new(MastodonAccountLookupAPIResponse)
+		if err := json.NewDecoder(resp.Body).Decode(lookupRespPayload); err != nil {
+			return err
+		}
+		srcUrl, _ := url.Parse(c.SiteUrl)
+		srcUrl.Path = fmt.Sprintf("/api/v1/accounts/%s/statuses", lookupRespPayload.Id)
+		fmt.Println(srcUrl.String())
 		c.SourceUrl = srcUrl.String()
 	}
 	if len(c.IconUrl) == 0 {
